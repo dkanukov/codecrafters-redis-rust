f833eb037e12f2f3049e23f024ab8e8da8a70bd3
dkanukov <dkanukov@ozon.ru>
2024-06-30 18:30:00 +0300

    concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -8,15 +9,17 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 let mut buffer = [0; 512];
-                loop {
-                    let read_count = stream.read(&mut buffer).unwrap();
-                    println!("{}", read_count);
+                thread::spawn(move || {
+                    loop {
+                        let read_count = stream.read(&mut buffer).unwrap();
+                        println!("{}", read_count);
 
-                    if read_count == 0 {
-                        break;
+                        if read_count == 0 {
+                            break;
+                        }
+                        stream.write_all(b"+PONG\r\n").unwrap();
                     }
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
