159b819c0d6820589f0b39a5f4f3e93991787709
dkanukov <dkanukov@ozon.ru>
2024-06-30 18:45:03 +0300

    spawn thread

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::thread;
+
+fn handle_command_with_thread(mut stream: TcpStream) {
+    thread::spawn(move || {
+        let mut buffer = [0; 512];
+        loop {
+            let read_count = stream.read(&mut buffer).unwrap();
+            println!("{}", read_count);
+
+            if read_count == 0 {
+                break;
+            }
+            stream.write_all(b"+PONG\r\n").unwrap();
+        }
+    });
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut buffer = [0; 512];
-                thread::spawn(move || {
-                    loop {
-                        let read_count = stream.read(&mut buffer).unwrap();
-                        println!("{}", read_count);
-
-                        if read_count == 0 {
-                            break;
-                        }
-                        stream.write_all(b"+PONG\r\n").unwrap();
-                    }
-                });
-            }
+            Ok(stream) => handle_command_with_thread(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
